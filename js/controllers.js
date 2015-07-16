36b91abed947008dd4f587d4babed369c9389b70
Craig Gjerdingen <gjer0005@umn.edu>
2015-07-16 17:17:23 -0500

    End of day Check-in

--- js/controllers.js
+++ js/controllers.js
@@ -1,9 +1,8 @@
 var myDemoApp = angular.module('myDemoApp', []);
 
-myDemoApp.controller('MyController', function MyController($scope) {
-$scope.mentor = {
-	'name' : 'Bill Gates',
-	'title' : 'Cheif Techology Officer',
-	'company' : 'Microsoft'
-	};
-});
+myDemoApp.controller('MyController', ['$scope', '$http', function MyController($scope, $http) {
+	$http.get('js/data.json').success(function(data) {
+		$scope.mentors = data;
+	});
+
+}]);
