8db153136e2e4a57e96e1cea0e22563bf17aadc8
Craig Gjerdingen <gjer0005@umn.edu>
2015-07-17 13:15:39 -0500

    Added Search form and order by filters

--- js/controllers.js
+++ js/controllers.js
@@ -3,6 +3,7 @@ var myDemoApp = angular.module('myDemoApp', []);
 myDemoApp.controller('MyController', ['$scope', '$http', function MyController($scope, $http) {
 	$http.get('js/data.json').success(function(data) {
 		$scope.mentors = data;
+		$scope.mentorOrder = 'name';
 	});
 
 }]);
