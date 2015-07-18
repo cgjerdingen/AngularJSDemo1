fe8f3f2d330a9eb9f47fd0b6c2786a95dbfe32ea
Craig Gjerdingen <gjer0005@umn.edu>
2015-07-17 20:54:28 -0500

    Add routing and detial partial view

--- js/controllers.js
+++ js/controllers.js
@@ -1,9 +1,27 @@
-var myDemoApp = angular.module('myDemoApp', []);
+var mentorControllers = angular.module('mentorControllers', ['ngAnimate']);
 
-myDemoApp.controller('MyController', ['$scope', '$http', function MyController($scope, $http) {
+mentorControllers.controller('ListController', ['$scope', '$http', function($scope, $http) {
 	$http.get('js/data.json').success(function(data) {
 		$scope.mentors = data;
-		$scope.mentorOrder = 'name';
+		/*$scope.mentorOrder = 'itemId';*/
+	});
+}]);
+
+mentorControllers.controller('DetailsController', ['$scope', '$http', '$routeParams', function($scope, $http, $routeParams) {
+	$http.get('js/data.json').success(function(data) {
+		$scope.mentors = data;
+		$scope.whichItem = $routeParams.itemId;
+
+		if ($routeParams.itemId > 0) {
+			$scope.prevItem = Number($routeParams.itemId)-1;
+		} else {
+			$scope.prevItem = $scope.mentors.length-1;
+		}
+		if ($routeParams.itemId < $scope.mentors.length-1) {
+			$scope.nextItem = Number($routeParams.itemId)+1;
+		} else {
+			$scope.nextItem = 0;
+		}
 	});
 
 }]);
